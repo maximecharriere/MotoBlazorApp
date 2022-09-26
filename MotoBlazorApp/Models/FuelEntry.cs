13f30749fcdad72926c544824c0cf2a1ce921ecd
maximecharriere <51918753+maximecharriere@users.noreply.github.com>
2022-09-26 23:27:45 +0200

    FuelEntry class

---
+++ MotoBlazorApp/Models/FuelEntry.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotoBlazorApp.Models
+{
+    public class FuelEntry
+    {
+        [Required]
+        [Range(0.0, Double.MaxValue, ErrorMessage = "{0} can't be negative")]
+        public double? Price { get; set; }
+
+        [Required]
+        [Range(0.0, Double.MaxValue, ErrorMessage = "{0} can't be negative")]
+        public double? Distance { get; set; }
+
+        [Required]
+        [Range(0.0, Double.MaxValue, ErrorMessage = "{0} can't be negative")]
+        public double? Quantity { get; set; }
+
+        public double? PriceByUnit
+        {
+            get
+            {
+                return Price / Quantity;
+            }
+
+        }
+
+    }
+}
